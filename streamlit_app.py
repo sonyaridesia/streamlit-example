7503dac9d917b58ddc2f73324bd4e5d6b9f50f21
sonyaridesia <115215336+sonyaridesia@users.noreply.github.com>
2023-12-11 16:44:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,110 +1,73 @@
 import streamlit as st
 import pandas as pd
-import base64
-import io
-import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def main():
-    st.title("💗 Data Processing Group Project 💗")
-    
-    st.markdown(
-        """
-        <style>
-            body {
-                background-color: #E0F4FF;
-            }
-            h1 {
-                background-color: #F9F9E0;
-                color: #C683D7;
-                text-align: center;
-                font-size: 48px;
-            }
-            h4 {
-                color: #C683D7;
-                text-align: center;
-                font-size: 34px;
-            }
-            ul {
-                list-style-type: none;
-                padding: 0;
-                text-align: center;
-            }
-            li {
-                color: #C683D7;
-            }
-            .upload-container {
-                width: 100%;
-                height: 60px;
-                line-height: 60px;
-                border-width: 1px;
-                border-style: dashed;
-                border-radius: 5px;
-                text-align: center;
-                margin: 10px;
-                background-color: #F9F9E0;
-            }
-            .output-container {
-                margin-top: 20px;
-            }
-            .header-section {
-                margin-bottom: 20px;
-            }
-            .preview-section {
-                margin-top: 20px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
+    st.title("Data Processing Group Project 📊")
     st.write("Nama Anggota:")
-    st.write("- Isky Dwi Aprilianto")
-    st.write("- Sonya Ridesia Hastari")
-    st.write("- Chaesa Namida Arumdapta")
-    st.write("- Tarum Widyasti Pertiwi")
-    st.write("- Vania Rahma Dewi")
+    st.write("- Isky Dwi Aprilianto-064002200006")
+    st.write("- Sonya Ridesia Hastari-064002200007")
+    st.write("- Chaesa Namida Arumdapta-064002200008")
+    st.write("- Tarum Widyasti Pertiwi-064002200027")
+    st.write("- Vania Rahma Dewi-064002200030")
 
-    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"], key="fileUploader")
+    # Upload CSV file through Streamlit
+    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
+    # Tombol "Process" untuk menampilkan data setelah file diunggah
     if uploaded_file is not None:
-        df = parse_contents(uploaded_file)
+        if st.button("Process CSV 🚀"):
+            # Read CSV file into DataFrame
+            df = pd.read_csv(uploaded_file)
 
-        if isinstance(df, pd.DataFrame):
-            st.header("🌸 Data Preview 🌸")
+            # Display basic information
+            st.write("<h2 style='color:#4a4a4a;'>Basic Information:</h2>", unsafe_allow_html=True)
+            st.write(f"<p style='color:#4a4a4a;'>Number of Rows: {df.shape[0]}</p>", unsafe_allow_html=True)
+            st.write(f"<p style='color:#4a4a4a;'>Number of Columns: {df.shape[1]}</p>", unsafe_allow_html=True)
+
+            # Display data types and missing values
+            st.write("<h2 style='color:#4a4a4a;'>Data Types and Missing Values:</h2>", unsafe_allow_html=True)
+            st.write(df.dtypes)
+            st.write(df.isnull().sum())
+
+            # Display descriptive statistics
+            st.write("<h2 style='color:#4a4a4a;'>Descriptive Statistics:</h2>", unsafe_allow_html=True)
+            st.write(df.describe())
+
+            # Display visualizations and other features as before...
+            st.write("<h2 style='color:#4a4a4a;'>Data Preview:</h2>", unsafe_allow_html=True)
             st.write(df)
 
-            st.header("🌼 Deskripsi Dataset 🌼")
-            st.code(df.describe().to_json(indent=2))
+            # Menambahkan fitur menghitung rata-rata
+            st.write("<h2 style='color:#4a4a4a;'>Rata-rata:</h2>", unsafe_allow_html=True)
+            for column in df.select_dtypes(include='number').columns:
+                mean_value = df[column].mean()
+                st.write(f"<p style='color:#4a4a4a;'>Mean of {column}: {mean_value}</p>", unsafe_allow_html=True)
 
-            st.header("🏵️ Boxplot untuk Deteksi Outlier 🏵️")
-            selected_column_boxplot = st.selectbox("Select a column for Boxplot", df.columns)
-            st.plotly_chart(update_boxplot(selected_column_boxplot, df))
+            # Menambahkan fitur menampilkan histogram
+            st.write("<h2 style='color:#4a4a4a;'>Histogram:</h2>", unsafe_allow_html=True)
+            for column in df.columns:
+                st.subheader(f"<p style='color:#4a4a4a;'>Histogram for {column}</p>", unsafe_allow_html=True)
+                st.bar_chart(df[column])
 
-            st.header("🌻 Histogram 🌻")
-            selected_column_histogram = st.selectbox("Select a column for Histogram", df.columns)
-            st.plotly_chart(update_histogram(selected_column_histogram, df))
+            # Menambahkan fitur menampilkan boxplot
+            st.write("<h2 style='color:#4a4a4a;'>Boxplot:</h2>", unsafe_allow_html=True)
+            for column in df.columns:
+                st.subheader(f"<p style='color:#4a4a4a;'>Boxplot for {column}</p>", unsafe_allow_html=True)
+                st.pyplot(plot_boxplot(df, column))
 
-def parse_contents(contents):
-    content_type, content_string = contents.type, contents.read()
-    decoded = base64.b64decode(content_string)
+def plot_boxplot(df, column):
+    plt.figure(figsize=(8, 6))
     
-    try:
-        if 'csv' in content_type:
-            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-            return df
-    except Exception as e:
-        st.error('Error loading CSV file')
-        st.write(e)
-        return None
-
-def update_boxplot(selected_column, df):
-    fig = px.box(df, y=selected_column)
-    return fig
-
-def update_histogram(selected_column, df):
-    fig = px.histogram(df, x=selected_column)
-    return fig
+    # Periksa apakah kolom berisi data numerik
+    if pd.api.types.is_numeric_dtype(df[column]):
+        sns.boxplot(x=df[column])
+        plt.title(f'Boxplot for {column}')
+    else:
+        plt.title(f'Cannot create boxplot for non-numeric column: {column}')
+    
+    return plt
 
 if __name__ == "__main__":
     main()
-
