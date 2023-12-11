33b01c251c7a7c65644be5ac8600720e36281409
sonyaridesia <115215336+sonyaridesia@users.noreply.github.com>
2023-12-11 16:56:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def main():
-    st.title("Data Processing Group Project 📊")
+    st.title("Data Processing Group Project")
     st.write("Nama Anggota:")
     st.write("- Isky Dwi Aprilianto-064002200006")
     st.write("- Sonya Ridesia Hastari-064002200007")
@@ -17,57 +17,55 @@ def main():
 
     # Tombol "Process" untuk menampilkan data setelah file diunggah
     if uploaded_file is not None:
-        if st.button("Process CSV 🚀"):
+        if st.button("Process CSV"):
             # Read CSV file into DataFrame
             df = pd.read_csv(uploaded_file)
 
             # Display basic information
-            st.write("<h2 style='color:#4a4a4a;'>Basic Information:</h2>", unsafe_allow_html=True)
-            st.write(f"<p style='color:#4a4a4a;'>Number of Rows: {df.shape[0]}</p>", unsafe_allow_html=True)
-            st.write(f"<p style='color:#4a4a4a;'>Number of Columns: {df.shape[1]}</p>", unsafe_allow_html=True)
+            st.write("## Basic Information:")
+            st.write(f"Number of Rows: {df.shape[0]}")
+            st.write(f"Number of Columns: {df.shape[1]}")
 
             # Display data types and missing values
-            st.write("<h2 style='color:#4a4a4a;'>Data Types and Missing Values:</h2>", unsafe_allow_html=True)
+            st.write("## Data Types and Missing Values:")
             st.write(df.dtypes)
             st.write(df.isnull().sum())
 
             # Display descriptive statistics
-            st.write("<h2 style='color:#4a4a4a;'>Descriptive Statistics:</h2>", unsafe_allow_html=True)
+            st.write("## Descriptive Statistics:")
             st.write(df.describe())
 
             # Display visualizations and other features as before...
-            st.write("<h2 style='color:#4a4a4a;'>Data Preview:</h2>", unsafe_allow_html=True)
+            st.write("## Data Preview:")
             st.write(df)
 
             # Menambahkan fitur menghitung rata-rata
-            st.write("<h2 style='color:#4a4a4a;'>Rata-rata:</h2>", unsafe_allow_html=True)
+            st.write("## Rata-rata:")
             for column in df.select_dtypes(include='number').columns:
                 mean_value = df[column].mean()
-                st.write(f"<p style='color:#4a4a4a;'>Mean of {column}: {mean_value}</p>", unsafe_allow_html=True)
+                st.write(f"Mean of {column}: {mean_value}")
 
             # Menambahkan fitur menampilkan histogram
-            st.write("<h2 style='color:#4a4a4a;'>Histogram:</h2>", unsafe_allow_html=True)
+            st.write("## Histogram:")
             for column in df.columns:
-                st.subheader(f"<p style='color:#4a4a4a;'>Histogram for {column}</p>", unsafe_allow_html=True)
+                st.subheader(f"Histogram for {column}")
                 st.bar_chart(df[column])
 
             # Menambahkan fitur menampilkan boxplot
-            st.write("<h2 style='color:#4a4a4a;'>Boxplot:</h2>", unsafe_allow_html=True)
+            st.write("## Boxplot:")
             for column in df.columns:
-                st.subheader(f"<p style='color:#4a4a4a;'>Boxplot for {column}</p>", unsafe_allow_html=True)
+                st.subheader(f"Boxplot for {column}")
                 st.pyplot(plot_boxplot(df, column))
 
-def plot_boxplot(df, column):
-    plt.figure(figsize=(8, 6))
-    
-    # Periksa apakah kolom berisi data numerik
-    if pd.api.types.is_numeric_dtype(df[column]):
-        sns.boxplot(x=df[column])
-        plt.title(f'Boxplot for {column}')
-    else:
-        plt.title(f'Cannot create boxplot for non-numeric column: {column}')
-    
-    return plt
+            # Menambahkan fitur one-hot encoding
+            st.write("## One-Hot Encoding:")
+            categorical_columns = df.select_dtypes(include='object').columns
+            selected_columns = st.multiselect("Select columns to one-hot encode", categorical_columns)
+
+            if selected_columns:
+                one_hot_df = pd.get_dummies(df, columns=selected_columns)
+                st.write("### One-Hot Encoded Data:")
+                st.write(one_hot_df)
 
 if __name__ == "__main__":
     main()
